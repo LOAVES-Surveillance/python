d51e3cc6321630b0f2b8a2dd6ca6fcfb91917498
kodekill <brian.falor@gmail.com>
2017-04-05 16:55:26 -0600

    Added time stamp and error exceptions.

--- Pi_Camera.py
+++ Pi_Camera.py
@@ -20,10 +20,11 @@ timmer_count = 10
 photo_number = 1
 reset_time = record_amount
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)         
-GPIO.setup(PIR, GPIO.IN)                         #Read output from PIR motion sensor
-date_string = time.strftime('%m_%d_%Y')          #Date for time stamp
-hour_string = time.strftime("%H:%M:%S")          #Hour and minuets for time stamp
+GPIO.setmode(GPIO.BOARD)                             #Use the regular pin identifiers. 
+GPIO.setup(PIR, GPIO.IN)                                  #Read output from PIR motion sensor
+date_string = time.strftime('%m_%d_%Y')           #Date for time stamp
+hour_string = time.strftime("%H:%M:%S")           #Time used for file naming
+standard_hour_string = time.strftime("%r")          #Time used for video time stamp overlay
 mypath = '/home/pi/LOAVES/pics/' + date_string + '/'
 
 if not os.path.isdir(mypath):
@@ -54,35 +55,51 @@ drive = GoogleDrive(gauth)                       # Set drive location using .jso
 
 
 # Main program:
-if __name__ == '__main__':
-        
-    while True:
-           i=GPIO.input(PIR)                       #set PIR as input
-           if i==0:                                #When output from motion sensor is LOW
-                 print "No intruders around"
-                 time.sleep(delay)
-                 
-           elif i==1:                              #When output from motion sensor is HIGH
-                 print "Intruder detected"
-                 my_file = (mypath +  str(hour_string) + '.h264')
-                 
-                 camera.start_recording(my_file)
-                 file2 = drive.CreateFile()
-                 
+try:
+	if __name__ == '__main__':
+		
+		while True:
+			i=GPIO.input(PIR)                       #set PIR as input
+			if i==0:                                #When output from motion sensor is LOW
+				print "No intruders around"
+				time.sleep(delay)
+				 
+			elif i==1:                              #When output from motion sensor is HIGH
+				print "Intruder detected"
+				my_file = (mypath +  str(hour_string) + '.h264')
+			 
+				camera.start_recording(my_file)
+				file2 = drive.CreateFile()
+			 
 
-                 print "Recording in progress"
-                 print "Will record for %s seconds." %(record_amount)
-                 
-                 while (record_amount >= 0):                #This will blink red to let you know it's
-                        record_amount -= 1                  #currently recording
-                        time.sleep(1)
-                        
-                 camera.stop_recording()
-                 time.sleep(.5)
-                 
-                 file2.SetContentFile(my_file)             #Set the recorded file to be uploaded   
-                 file2.Upload()                            #upload file to google drive
-                         
-                 hour_string = time.strftime("%H:%M:%S")   #reset time so we don't overwrite exisiting files
-                 record_amount = reset_time                #reset the record time so it doesn't stay at
-                 time.sleep(.5)                            #0 seconds and keep looping 1 sec videos
+				print "Recording in progress"
+				print "Will record for %s seconds." %(record_amount)
+			 
+				while (record_amount >= 0):                #This will blink red to let you know it's
+					record_amount -= 1                  			#currently recording
+					time.sleep(1)
+					standard_hour_string = time.strftime("%r")
+					camera.annotate_text = standard_hour_string
+					
+				camera.stop_recording()
+				time.sleep(.5)
+			 
+				print "Pushing saved video to Google Drive"        
+				file2.SetContentFile(my_file)             #Set the recorded file to be uploaded   
+				file2.Upload()                            #upload file to google drive
+					 
+				hour_string = time.strftime("%H:%M:%S")   #reset time so we don't overwrite exisiting files
+				standard_hour_string = time.strftime("%r")
+			 
+				record_amount = reset_time                #reset the record time so it doesn't stay at
+				time.sleep(.5)                            #0 seconds and keep looping 1 sec videos
+				print "Done!\n\n"
+				 
+except KeyboardInterrupt:
+        print "Program Terminated \n"
+
+except:
+	print "Other Error Occurred"
+        
+finally:
+        GPIO.cleanup()
